6ab0a2d6a9a31b76e9b62ad1c5aff4619576a4f3
Jan Lasota <jlasota@teamworks.com>
2025-07-07 00:31:10 -0500

    make sure empty state panel fills full screen

--- src/features/Dashboard/Dashboard.tsx
+++ src/features/Dashboard/Dashboard.tsx
@@ -138,7 +138,7 @@ const Dashboard = () => {
 
   return (
     <>
-      <div className="flex flex-col p-4">
+      <div className="flex flex-col p-4 h-full">
         <div className="flex justify-between items-center mb-4">
           {/* Header */}
           <div className="flex items-center gap-2 text-3xl">
