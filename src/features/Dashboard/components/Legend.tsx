7eea79da9af0205bfc59b63c4c7f88fc9afecb36
Jan Lasota <jlasota@teamworks.com>
2025-07-07 00:32:20 -0500

    inline style colors no work in tailwind

--- src/features/Dashboard/components/Legend.tsx
+++ src/features/Dashboard/components/Legend.tsx
@@ -3,7 +3,10 @@ const Legend = ({ items }: { items: { label: string; color: string }[] }) => {
     <div className="flex gap-4">
       {items.map((item) => (
         <div key={item.label} className="flex items-center gap-2">
-          <div className={`w-2 h-2 rounded-sm bg-[${item.color}]`}></div>
+          <div
+            className="w-2 h-2 rounded-sm"
+            style={{ backgroundColor: item.color }}
+          ></div>
           <span className="text-sm text-black">{item.label}</span>
         </div>
       ))}
