870e60bd6b9516db0f485bbc3345eb15cc1697b4
Jan Lasota <jlasota@teamworks.com>
2026-04-15 12:29:11 -0500

    yes

--- src/features/Dashboard/components/FoodCard.tsx
+++ src/features/Dashboard/components/FoodCard.tsx
@@ -36,7 +36,7 @@ const FoodCard = ({
       tabIndex={0}
     >
       <div className="flex justify-between">
-        <div className="flex items-center gap-2 mb-2 mt-4">
+        <div className="flex items-center gap-2 mb-4">
           {renderImage ? (
             <img
               src={food.image}
